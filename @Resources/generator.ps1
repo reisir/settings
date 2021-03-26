c095b23e638a7bc689680f4644ada15152e6bf34
sceleri <sceleri@zeppe.li>
2021-03-27 01:37:15 +0200

    Skin generation beginnings

--- @Resources/generator.ps1
+++ @Resources/generator.ps1
@@ -8,6 +8,7 @@ $settingsFile = $RmAPI.VariableStr("SettingsFile")
 
 # directories & files
 $testfile = "$($RmAPI.VariableStr('@'))test.inc"
+$generatedSkinFile = "$($RmAPI.VariableStr('ROOTCONFIGPATH'))settings\settings.ini"
 $categoriesDir = "$($RmAPI.VariableStr('ROOTCONFIGPATH'))settings\categories\"
 $settingsFilePath = "$($RmAPI.VariableStr('SKINSPATH'))$($skin)\@Resources\$($settingsFile)"
 
@@ -19,28 +20,43 @@ $variableIdentifier = ";;"
 # Regex patterns
 $categoryPattern = '(?s-m);@(.*?)(?=;@|$)'
 $categoryTitlePattern = '(?s-m)(?<=;@)(.*?)(?=\n)'
-$variablePattern = '(?s-m);;(.*?)\n(?!;).*?\n'
+# old pattern, doesn't lookahead for EOF, might be relevant
+# variablePattern = '(?s-m);;(.*?)\n(?!;).*?\n';
+$variablePattern = '(?s-m);;(.*?)\n(?![;$]).*?\n';
 $variableTypePattern = '(?<=Type=)(.*?)(?=\n)'
 $variableDefaultValuePattern = '(?<=DefaultValue=)(.*?)(?=\n)'
 $variableNamePattern = '(?<=Name=)(.*?)(?=\n)'
 
-## Full settings object
-$settings = @()
-$categories = @()
-
 function Construct {
 
-    Filter-Settings > $testfile
+    # Read settings file
+    $file = Get-Content $settingsFilePath -Raw
+
+    # Filter settings file into staggered array
+    $settings = Filter-Settings $file
+
+    # Variable to hold generated .ini
+    $ini = ""
+
+    # Construct categories
+    $i = 0
+    foreach ($category in $settings) {
+        $ini += New-Category $category $i
+        $i++
+    }
+
+    $ini > $generatedSkinFile
 
 }
 
-function Filter-Settings {
+function Filter-Settings($settingsFileContent) {
 
-    # Read settings file
-    $settingsFileContent = Get-Content $settingsFilePath -Raw
     $categories = $settingsFileContent | Select-String -Pattern $categoryPattern -AllMatches
 
+    $settings = @()
+    
     foreach ($match in $categories.Matches) {
+
         $title = $match | Select-String -Pattern $categoryTitlePattern
         $category = @($title.Matches[0].value)
         $variables = $match | Select-String -Pattern $variablePattern -AllMatches
@@ -49,11 +65,16 @@ function Filter-Settings {
             $type = $variable | Select-String -Pattern $variableTypePattern
             $name = $variable | Select-String -Pattern $variableNamePattern
             $defaultvalue = $variable | Select-String -Pattern $variableDefaultValuePattern
-            $variable = @{'type' = $type.Matches[0]; 'name' = $name.Matches[0]; 'defaultValue' = $defaultvalue.Matches[0]}
-            $category += $variable
+
+            $type = Filter-Newline $type.Matches[0].value
+            $name = Filter-Newline $name.Matches[0].value
+            $defaultvalue = Filter-Newline $defaultvalue.Matches[0].value
+
+            $variable = @{'Type' = $type; 'Name' = $name; 'DefaultValue' = $defaultvalue}
+            $category += , $variable
         }
 
-        $settings += $category
+        $settings += , $category
 
     }
 
@@ -62,16 +83,36 @@ function Filter-Settings {
 }
 
 function New-Category($category, $i) {
-    $categoryFile = "$($categoriesDir)$($i).inc"
+
+    $variables = $category[1..($category.Length)]
+
+    $RmAPI.Log("Count: $($variables.Length)")
+
+    # List all variables to $text
+    $text = ""
+
+    foreach ($var in $variables) {
+        $text += "$($var.Name) "
+    }
 
     $categoryIni = @"
-    [Title]
-    Meter=String
-    Text=$($category[0])
-    MeterStyle=TextStyle
-    
+[Title$($i)]
+Meter=String
+Text=$($text)
+MeterStyle=TextStyle
+
 "@
 
-    $categoryIni > $categoryFile
-    
+    return $categoryIni
+
+}
+
+# no worky
+function Filter-Newline($s) {
+
+    $filter = "`n"
+    $s = $s -replace([System.Environment]::NewLine,"")
+
+    return $s 
+
 }
